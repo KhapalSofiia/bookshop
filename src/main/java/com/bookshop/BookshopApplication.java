38fa36f53b8413b569422640e359e35a722d59c1
sofiia <sofiia.khapal@nure.ua>
2026-02-17 23:52:56 +0100

    Book logics

--- src/main/java/com/bookshop/BookshopApplication.java
+++ src/main/java/com/bookshop/BookshopApplication.java
@@ -1,11 +1,30 @@
 package com.bookshop;
 
+import com.bookshop.model.Book;
+import com.bookshop.repository.BookRepository;
+import java.math.BigDecimal;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookshopApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookshopApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner demo(BookRepository repository) {
+        return args -> {
+            Book book = new Book();
+            book.setTitle("It");
+            book.setAuthor("Stephen King");
+            book.setIsbn("1111");
+            book.setPrice(new BigDecimal("200"));
+            repository.save(book);
+
+            System.out.println("The book successfully added to db");
+        };
     }
 }
---
+++ src/main/java/com/bookshop/repository/impl/BookRepositoryImpl.java
@@ -0,0 +1,55 @@
+package com.bookshop.repository.impl;
+
+import com.bookshop.exception.DataProcessingException;
+import com.bookshop.model.Book;
+import com.bookshop.repository.BookRepository;
+import jakarta.persistence.EntityManagerFactory;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookRepositoryImpl implements BookRepository {
+    private final EntityManagerFactory entityManagerFactory;
+
+    @Autowired
+    public BookRepositoryImpl(EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
+    }
+
+    @Override
+    public Book save(Book book) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = entityManagerFactory.unwrap(SessionFactory.class).openSession();
+            transaction = session.beginTransaction();
+            session.persist(book);
+            transaction.commit();
+            return book;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can not add new book.", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (Session session = entityManagerFactory.unwrap(SessionFactory.class).openSession()) {
+            Query<Book> query = session.createQuery("from Book", Book.class);
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can not find all books.", e);
+        }
+    }
+}
