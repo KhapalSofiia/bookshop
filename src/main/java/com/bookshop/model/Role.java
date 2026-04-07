65bbc17c3222933eed88dd782ec039b58755ebbf
Khapal Sofiia <sofiia.khapal@nure.ua>
2026-04-07 13:25:01 +0200

    004-Іmplementation role logik and access to methods by role

---
+++ src/main/java/com/bookshop/model/Role.java
@@ -0,0 +1,36 @@
+package com.bookshop.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    @Enumerated(EnumType.STRING)
+    private RoleNameEnum role;
+
+    public String getAuthority() {
+        return role.name();
+    }
+
+    public enum RoleNameEnum {
+        ADMIN,
+        USER
+    }
+}
