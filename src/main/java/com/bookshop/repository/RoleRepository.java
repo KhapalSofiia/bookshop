3962f9d85e0af87017648644187b6a5af4c9aeb7
Khapal Sofiia <sofiia.khapal@nure.ua>
2026-04-09 12:43:31 +0200

    012-Added method to find the Role by its name

--- src/main/java/com/bookshop/repository/RoleRepository.java
+++ src/main/java/com/bookshop/repository/RoleRepository.java
@@ -1,7 +1,10 @@
 package com.bookshop.repository;
 
 import com.bookshop.model.Role;
+import com.bookshop.model.enums.RoleNameEnum;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleNameEnum name);
 }
