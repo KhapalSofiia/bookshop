312300671308694ffd8e277e09749b6dc8df71cb
Khapal Sofiia <sofiia.khapal@nure.ua>
2026-04-09 14:52:15 +0200

    001-Create entity for categories

---
+++ src/main/java/com/bookshop/model/Category.java
@@ -0,0 +1,27 @@
+package com.bookshop.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    //TODO ADD TO USER private Set<Category> categories = new HashSet<>()
+}
