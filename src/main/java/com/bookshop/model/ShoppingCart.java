79f7ab937830683fd552c5f98b870b77b45c09b7
Khapal Sofiia <sofiia.khapal@nure.ua>
2026-04-10 14:01:59 +0200

    001-Craete entities for CartItem and ShoppingCart

---
+++ src/main/java/com/bookshop/model/ShoppingCart.java
@@ -0,0 +1,36 @@
+package com.bookshop.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "shopping_cart")
+public class ShoppingCart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(
+            mappedBy = "shoppingCart",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private Set<CartItem> cartItems = new HashSet<>();
+}
