2976eefb4a23d19bb58a04e8c7c4b9ae7736c95e
Khapal Sofiia <sofiia.khapal@nure.ua>
2026-04-17 14:18:17 +0200

    008-Added logik of creatend the ShoppingCart by registration

--- src/main/java/com/bookshop/service/impl/UserServiceImpl.java
+++ src/main/java/com/bookshop/service/impl/UserServiceImpl.java
@@ -4,7 +4,9 @@ import com.bookshop.dto.UserDto;
 import com.bookshop.dto.UserRegistrationDto;
 import com.bookshop.exception.RegistrationException;
 import com.bookshop.mapper.UserMapper;
+import com.bookshop.model.ShoppingCart;
 import com.bookshop.model.User;
+import com.bookshop.repository.ShoppingCartRepository;
 import com.bookshop.repository.UserRepository;
 import com.bookshop.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +16,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final ShoppingCartRepository shoppingCartRepository;
     private final UserMapper userMapper;
 
     public UserDto registration(UserRegistrationDto userRegistrationDto) {
@@ -25,6 +28,10 @@ public class UserServiceImpl implements UserService {
 
         User user = userMapper.toModel(userRegistrationDto);
         userRepository.save(user);
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.setUser(user);
+        shoppingCartRepository.save(shoppingCart);
+
         return userMapper.toUserDto(user);
     }
 }
