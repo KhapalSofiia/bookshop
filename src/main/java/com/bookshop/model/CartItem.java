1907ebe82123c034e5480a003fdd93949bc70f5f
Khapal Sofiia <sofiia.khapal@nure.ua>
2026-04-17 14:16:52 +0200

    006-Edited entities

--- src/main/java/com/bookshop/model/CartItem.java
+++ src/main/java/com/bookshop/model/CartItem.java
@@ -1,5 +1,6 @@
 package com.bookshop.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,12 +9,14 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "cart_item")
+@NoArgsConstructor
 public class CartItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,5 +30,10 @@ public class CartItem {
     @JoinColumn(name = "book_id", nullable = false)
     private Book book;
 
+    @Column(nullable = false)
     private int quantity;
+
+    public CartItem(Long id) {
+        this.id = id;
+    }
 }
--- src/main/java/com/bookshop/model/ShoppingCart.java
+++ src/main/java/com/bookshop/model/ShoppingCart.java
@@ -6,25 +6,29 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "shopping_cart")
+@SQLDelete(sql = "UPDATE shopping_cart SET is_deleted = true WHERE id = ?")
+@SQLRestriction("is_deleted = false")
 public class ShoppingCart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @OneToMany(
@@ -33,4 +37,6 @@ public class ShoppingCart {
             orphanRemoval = true
     )
     private Set<CartItem> cartItems = new HashSet<>();
+
+    private boolean isDeleted = false;
 }
