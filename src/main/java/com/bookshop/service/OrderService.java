26ef9f168f8ff7ef2ce2dfe05603eea928250937
Khapal Sofiia <sofiia.khapal@nure.ua>
2026-04-28 13:54:00 +0200

    007-Create service for orders

---
+++ src/main/java/com/bookshop/service/OrderService.java
@@ -0,0 +1,19 @@
+package com.bookshop.service;
+
+import com.bookshop.dto.OrderDto;
+import com.bookshop.dto.OrderItemDto;
+import com.bookshop.dto.PlaceOrderDto;
+import com.bookshop.dto.UpdateOrderStatusDto;
+import java.util.List;
+
+public interface OrderService {
+    List<OrderDto> getAllOrders(String email);
+
+    OrderDto placeOrder(String email, PlaceOrderDto orderDto);
+
+    OrderDto updateOrderStatus(UpdateOrderStatusDto updateOrderStatus, Long orderId);
+
+    List<OrderItemDto> getOrderItems(Long orderId);
+
+    OrderItemDto getOrderItem(Long orderId, Long orderItemId);
+}
---
+++ src/main/java/com/bookshop/service/impl/OrderServiceImpl.java
@@ -0,0 +1,123 @@
+package com.bookshop.service.impl;
+
+import com.bookshop.dto.OrderDto;
+import com.bookshop.dto.OrderItemDto;
+import com.bookshop.dto.PlaceOrderDto;
+import com.bookshop.dto.UpdateOrderStatusDto;
+import com.bookshop.exception.EntityNotFoundException;
+import com.bookshop.mapper.OrderItemMapper;
+import com.bookshop.mapper.OrderMapper;
+import com.bookshop.model.CartItem;
+import com.bookshop.model.Order;
+import com.bookshop.model.OrderItem;
+import com.bookshop.model.ShoppingCart;
+import com.bookshop.model.User;
+import com.bookshop.model.enums.OrderStatus;
+import com.bookshop.repository.OrderItemRepository;
+import com.bookshop.repository.OrderRepository;
+import com.bookshop.repository.UserRepository;
+import com.bookshop.service.OrderService;
+import jakarta.transaction.Transactional;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
+    private final UserRepository userRepository;
+    private final OrderRepository orderRepository;
+    private final OrderMapper orderMapper;
+    private final OrderItemMapper orderItemMapper;
+    private final OrderItemRepository orderItemRepository;
+    private final ShoppingCartServiceImpl shoppingCartServiceImpl;
+
+    @Override
+    @Transactional
+    public List<OrderDto> getAllOrders(String email) {
+        User user = userRepository.findByEmail(email).orElseThrow(
+                () -> new EntityNotFoundException("User with email " + email + " not found")
+        );
+
+         return orderRepository.findByUser(user)
+                .stream()
+                .map(orderMapper::toDto)
+                .toList();
+    }
+
+    @Override
+    @Transactional
+    public OrderDto placeOrder(String email, PlaceOrderDto orderDto) {
+        ShoppingCart shoppingCart = shoppingCartServiceImpl.getShoppingCartByEmail(email);
+
+        Order order = new Order();
+        order.setUser(shoppingCart.getUser());
+        order.setShippingAddress(orderDto.getShippingAddress());
+        order.setStatus(OrderStatus.PENDING);
+        order.setOrderDate(LocalDateTime.now());
+
+        Set<OrderItem> orderItems = shoppingCart.getCartItems().stream()
+                .map(cartItem -> convertToOrderItem(cartItem, order))
+                .collect(Collectors.toSet());
+        order.setOrderItems(orderItems);
+
+        BigDecimal total = orderItems.stream()
+                .map(item -> item.getPrice()
+                        .multiply(BigDecimal.valueOf(item.getQuantity())))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        order.setTotal(total);
+
+        orderRepository.save(order);
+        shoppingCart.getCartItems().clear();
+
+        return orderMapper.toDto(order);
+    }
+
+    @Override
+    @Transactional
+    public OrderDto updateOrderStatus(UpdateOrderStatusDto updateOrderStatus, Long orderId) {
+        Order order = orderRepository.findById(orderId).orElseThrow(() ->
+                new EntityNotFoundException("Order with id " + orderId + " not found"));
+
+        order.setStatus(updateOrderStatus.getStatus());
+
+        return orderMapper.toDto(orderRepository.save(order));
+    }
+
+    @Override
+    @Transactional
+    public List<OrderItemDto> getOrderItems(Long orderId) {
+        return orderRepository.findById(orderId)
+                .stream()
+                .flatMap(order -> order.getOrderItems().stream())
+                .map(orderItemMapper::toDto)
+                .toList();
+    }
+
+    @Override
+    @Transactional
+    public OrderItemDto getOrderItem(Long orderId, Long orderItemId) {
+        Order order = orderRepository.findById(orderId).orElseThrow(() -> new EntityNotFoundException(
+                "Order with id " + orderId + " not found"));
+        OrderItem orderItem = orderItemRepository.getByOrderAndId(order, orderItemId)
+                .orElseThrow(() -> new EntityNotFoundException(
+                        "OrderItem with id " + orderItemId + " not found"));
+
+        return orderItemMapper.toDto(orderItem);
+    }
+
+    private OrderItem convertToOrderItem(CartItem cartItem, Order order) {
+        OrderItem orderItem = new OrderItem();
+
+        orderItem.setOrder(order);
+        orderItem.setBook(cartItem.getBook());
+        orderItem.setQuantity(cartItem.getQuantity());
+        orderItem.setPrice(cartItem.getBook().getPrice());
+
+        return orderItem;
+    }
+}
